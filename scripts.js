a6aa48758448b5d8c9824549eaeb87880dfd76bb
Anil Anar <anilanar@hotmail.com>
2020-07-26 17:49:48 +0200

    Try using absolute path for nps scripts

--- scripts.js
+++ scripts.js
@@ -1,20 +1,29 @@
 const path = require("path");
 
-console.log(`HYGEN_TMPLS=${path.resolve(__dirname, "./templates")}`);
-
 module.exports = {
   scripts: {
-    new: `HYGEN_TMPLS=${path.resolve(__dirname, "./templates")} hygen lib new`,
-    add: `HYGEN_TMPLS=${path.resolve(__dirname, "./templates")} hygen lib add`,
-    build: "lerna run clean && lerna run build && lerna run build:cjs",
-    lint: "lerna run lint",
-    test: "lerna run test",
+    new: `HYGEN_TMPLS=${path.resolve(__dirname, "./templates")} ${bin(
+      "hygen"
+    )} lib new`,
+    add: `HYGEN_TMPLS=${path.resolve(__dirname, "./templates")} ${bin(
+      "hygen"
+    )} lib add`,
+    build: `${bin(
+      "lerna"
+    )} run clean && lerna run build && lerna run build:cjs`,
+    lint: `${bin("lerna")} run lint`,
+    test: `${bin("lerna")} run test`,
     commit: `${path.resolve(__dirname, "./commit.js")}`,
-    version:
-      "GH_TOKEN=$GHT lerna version --conventional-commits --create-release github",
-    "version:pre": 'nps "version --conventional-prerelease"',
-    "version:grad": 'nps "version --conventional-graduate"',
-    publish: "lerna publish from-git",
-    prepare: "nps build",
+    version: `GH_TOKEN=$GHT ${bin(
+      "lerna"
+    )} version --conventional-commits --create-release github`,
+    "version:pre": `${bin("nps")} "version --conventional-prerelease"`,
+    "version:grad": `${bin("nps")} "version --conventional-graduate"`,
+    publish: `${bin("lerna")} publish from-git`,
+    prepare: `${bin("nps")} build`,
   },
 };
+
+function bin(name) {
+  return path.resolve(__dirname, "node_modules/.bin", name);
+}
