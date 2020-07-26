296141c791f1e03c3a7ca1f407ee9a15d659ccc1
Anil Anar <anilanar@hotmail.com>
2020-07-26 02:44:11 +0200

    Fix wrong var

--- run.js
+++ run.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 const execa = require("execa");
-const p = require("path");
+const path = require("path");
 
 execa
   .command(
-    `${path.resolve(__dirname, "./node_modules/.bin/nps")} -c ${p.resolve(
+    `${path.resolve(__dirname, "./node_modules/.bin/nps")} -c ${path.resolve(
       __dirname,
       "./scripts.js"
     )} ${process.argv.slice(2).join(" ")}`,
