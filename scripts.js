bf5e84ca738852f85a8cadefdf96a9231feaed7c
Anil Anar <anilanar@hotmail.com>
2020-08-06 11:08:26 +0200

    Fix scripts calling other nps scripts

--- scripts.js
+++ scripts.js
@@ -1,26 +1,48 @@
 const path = require("path");
 
+const new_ = `HYGEN_TMPLS=${path.resolve(__dirname, "./templates")} ${bin(
+  "hygen"
+)} lib new`;
+
+const add = `HYGEN_TMPLS=${path.resolve(__dirname, "./templates")} ${bin(
+  "hygen"
+)} lib add`;
+
+const build = `${bin(
+  "lerna"
+)} run clean && lerna run build && lerna run build:cjs`;
+
+const lint = `${bin("lerna")} run lint`;
+
+const test = `${bin("lerna")} run test`;
+
+const commit = `${path.resolve(__dirname, "./commit.js")}`;
+
+const version = `GH_TOKEN=$GHT ${bin(
+  "lerna"
+)} version --conventional-commits --create-release github`;
+
+const version_pre = `${version} --conventional-prerelease"`;
+
+const version_grad = `${version} --conventional-graduate"`;
+
+const publish = `${bin("lerna")} publish from-git`;
+
+const prepare = build;
+
 module.exports = {
   scripts: {
-    new: `HYGEN_TMPLS=${path.resolve(__dirname, "./templates")} ${bin(
-      "hygen"
-    )} lib new`,
-    add: `HYGEN_TMPLS=${path.resolve(__dirname, "./templates")} ${bin(
-      "hygen"
-    )} lib add`,
-    build: `${bin(
-      "lerna"
-    )} run clean && lerna run build && lerna run build:cjs`,
-    lint: `${bin("lerna")} run lint`,
-    test: `${bin("lerna")} run test`,
-    commit: `${path.resolve(__dirname, "./commit.js")}`,
-    version: `GH_TOKEN=$GHT ${bin(
-      "lerna"
-    )} version --conventional-commits --create-release github`,
-    "version:pre": `${bin("nps")} "version --conventional-prerelease"`,
-    "version:grad": `${bin("nps")} "version --conventional-graduate"`,
-    publish: `${bin("lerna")} publish from-git`,
-    prepare: `${bin("nps")} build`,
+    new: new_,
+    add,
+    build,
+    lint,
+    test,
+    commit,
+    version,
+    "version:pre": version_pre,
+    "version:grad": version_grad,
+    publish,
+    prepare,
   },
 };
 
